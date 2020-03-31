0a58bcef8637f9152bd69c7f68bfd6d5aeba07ce
a24ma <62923767+a24ma@users.noreply.github.com>
2020-03-31 17:22:24 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# setup_ubuntu
+# Setup Ubuntu
+
+Ubuntu を bash で設定するツール。
+
+以下のことが自動(または半自動)で設定できる。
+
+* homedir 下に作業用ディレクトリを作成(パラメータ top_id で指定)
+* proxy の設定(.profile, apt を設定)
+* apt の最新化
+* Windows 共有ディレクトリの設定
+* SSH, Git の設定 (Git への鍵登録は手動)
+* 各言語設定
+  * C
+  * python (pyenv+virtualenv): py2 と py3 の仮想環境をデフォルトで作成(py3 が global)
+  * go
+* VSCode の設定 (設定共有は手動)
+* Chromium の設定 (アカウント設定は手動)
+
+バックアップ処理以外は冪等性を持つ。
+以下のファイルは、内容に変更がある場合にはバックアップを取る(`~/<top_id>/backup`下)。
+
+* ~/.profile
+* ~/.bashrc
+* /etc/apt/apt.conf
+
+# Installation
+
+```bash
+cd "<directory>"              # TO BE EDITED
+proxy="http://<proxy>:<port>" # TO BE EDITED
+
+export http_proxy=$proxy
+export https_proxy=$proxy
+sudo cat <<EOF > /etc/apt/apt.conf
+Acquire::http::proxy "$proxy";
+Acquire::https::proxy "$proxy";
+EOF
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y git
+git clone https://github.com/a24ma/setup_ubuntu
+```
+
+# How to use
+
+1. setup.conf のパラメータを設定。
+2. `./setup.bash` を実行(setup.conf のあるディレクトリで実行すること)。
+3. 再実行でスキップする場合は `enable_xxx=n` に設定(or コメントアウト)すること。
---
+++ setup.bash
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+is_executed_by_source() { return $([[ "$-" =~ "i" ]]); }
+if is_executed_by_source; then
+    echo "Do NOT execute by source."
+    exit 1
+fi
+
+main() {
+    (
+        sudo echo
+        export baksuf=".bak_$(date '+%Y%m%d_%H%M%S')"
+        read_conf
+        setup top_dir
+        setup proxy
+        setup apt
+        setup share
+        setup c
+        setup ssh
+        setup git
+        setup python
+        setup go
+        setup vscode
+        setup chromium
+        
+        echo "Succeeded. Restart the shell or run 'source ~/.profile'".
+    )
+}
+
+on_failed() { echo "failed on $1"; rm -f out; exit 1; }
+require() { if [[ -z "$(eval "echo \$${1:?}")" ]]; then on_failed "undefined:$1"; fi }
+
+edit_with_backup() {
+    require top_id
+    usage="usage: $0 <edit_path> <setup_id> <backup_id> <comment_str> (REQUIRE: 'out' is required to be added to edit_path.)"
+    edit_path="${1:?$usage}"
+    setup_id="${2:?$usage}"
+    backup_id="${3:?$usage}"
+    comment_str="${4:?$usage}"
+    if [ ! -f out ]; then echo "[ERROR] 'out' not found."; return 1; fi
+    tag="${top_id}_${setup_id}"
+    sed "/^${comment_str} <${tag}>/,/^${comment_str}<\/${tag}>/d" "$edit_path" > _out
+    cat out >> _out
+    latest_backup="$(ls -t ~/$top_id/backup/${backup_id}* | head -1)"
+    if diff -q "$edit_path" "$latest_backup"; then :; else
+        cp -f "$edit_path" ~/$top_id/backup/${backup_id}${baksuf}
+    fi
+    cp -f _out "$edit_path"
+    rm -f out _out
+}
+
+read_conf() {
+    set -e
+    if [ ! -f "setup.conf" ]; then on_failed "'setup.conf' not found"; fi
+    while read line; do
+        if [[ -z "$line" ]]; then continue; fi
+        if [[ "${line:0:1}" == "#" ]]; then continue; fi
+        echo "Config: $(eval "echo $line")"
+        export "$(eval "echo $line")"
+    done < "setup.conf"
+}
+
+setup() {
+    echo ""
+    command="setup_${1:?usage: $0 <setup_id>}"
+    enable="$(eval "echo \$enable_${command}")"
+    if [[ "${enable:0:1}" != "y" ]]; then
+        echo "[WARNING] Skip $command."
+        return
+    fi
+    echo "[INFO] Run $command"
+    $command || on_failed "$command"
+}
+
+setup_top_dir() {
+    require top_id
+    set -e
+    mkdir -p ~/$top_id
+    mkdir -p ~/$top_id/backup
+}
+
+setup_proxy() {
+    require top_id
+    require proxy
+    set -e
+    # Setup env
+    cat <<EOF >> out
+# <${top_id}_setup>
+export http_proxy='$proxy'
+export https_proxy='$proxy'
+export ftp_proxy='$proxy'
+export HTTP_PROXY='$proxy'
+export HTTPS_PROXY='$proxy'
+export FTP_PROXY='$proxy'
+export ALL_PROXY='$proxy'
+unset_proxy() {
+    export http_proxy=''
+    export https_proxy=''
+    export ftp_proxy=''
+    export HTTP_PROXY=''
+    export HTTPS_PROXY=''
+    export FTP_PROXY=''
+    export ALL_PROXY=''
+}
+# </${top_id}_setup>
+EOF
+    edit_with_backup ~/.profile setup profile "#"
+
+    ## Setup apt
+    cat <<EOF > out
+// <${top_id}_setup>
+Acquire::http::proxy "$proxy";
+Acquire::https::proxy "$proxy";
+// </${top_id}_setup>
+EOF
+    sudo touch /etc/apt/apt.conf
+    edit_with_backup /etc/apt/apt.conf setup apt.conf "\/\/"
+}
+
+setup_apt() {
+    set -e
+    sudo apt -y update
+    sudo apt -y upgrade
+    sudo apt -y autoremove
+}
+
+setup_share() {
+    set -e
+    sudo apt install -y cifs-utils
+    sudo mkdir -p /mnt/Share
+    clear
+    mount -v | grep "/mnt/Share" || \
+        sudo mount -t cifs -o uid=${myname},gid=${myname},vers=3.0,username=${smbid} $smbaddr /mnt/Share
+    rm -f ~/Share
+    ln -s /mnt/Share ~/Share
+    cat <<EOF > ~/connect_share.bash
+#!/usr/bin/env bash
+mount -v | grep "/mnt/Share" || sudo mount -t cifs -o uid=${myname},gid=${myname},vers=3.0,username=${smbid} $smbaddr /mnt/Share
+EOF
+    chmod a+x ~/connect_share.bash
+}
+
+setup_c() {
+    sudo apt install -y build-essential cmake libssl-dev
+}
+
+setup_ssh() {
+    require deviceid
+    sshpath=~/.ssh/rsa_$deviceid
+    if [ ! -e $sshpath ]; then
+        ssh-keygen -t rsa -b 4096 -C "$deviceid" -f $sshpath
+    fi
+}
+
+setup_git() {
+    sudo apt install -y git
+    echo "[INFO:SSH] Login GitHub and click Profile>Settings>SSH keys."
+    echo "[INFO:SSH] Use public key: $(cat ${sshpath}.pub)"
+}
+
+setup_python() {
+    require top_id
+    if [ ! -e ~/.pyenv ]; then
+        git clone https://github.com/pyenv/pyenv.git ~/.pyenv        
+    fi
+    if [ ! -e ~/.pyenv/plugins/pyenv-virtualenv ]; then
+        git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+    fi
+    cat <<EOF >> out
+# <${top_id}_python>
+export PYENV_ROOT="\$HOME/.pyenv"
+export PATH="\$PYENV_ROOT/bin:\$PATH"
+eval "\$(pyenv init -)"
+eval "\$(pyenv virtualenv init -)"
+# </${top_id}_python>
+EOF
+    edit_with_backup ~/.bashrc python bashrc "#"
+    source ~/.bashrc
+    pyenv -v
+
+    sudo apt install -y \
+        libffi-dev libssl-dev zlib1g-dev liblzma-dev \
+        libbz2-dev libreadline-dev libsqlite3-dev
+    for v in 2 3; do
+        pyv="$(pyenv install --list | tr -d " " | grep -v [-ab] | grep "^$v" | tail -1)"
+        if (pyenv versions | grep "${pyv:?'pyv not defined.'}"); then 
+            echo "[INFO:PYTHON] py$v is alread installed."
+        else 
+            echo "[INFO:PYTHON] Install py$v (it takes a little time...)"
+            pyenv install $pyv
+        fi
+        pyenv uninstall -f py$v
+        pyenv virtualenv $pyv py$v
+    done
+    pyenv global py3
+    printf "[INFO:PYTHON] Global version: "; pyenv version
+    echo "[INFO:PYTHON] Installed versions: "; pyenv versions
+}
+
+setup_go() {
+    sudo snap set system proxy.http="$proxy"
+    sudo snap set system proxy.https="$proxy"
+    sudo snap install go --classic
+    sudo snap refresh go
+    go version
+}
+
+setup_vscode() {
+    sudo snap set system proxy.http="$proxy"
+    sudo snap set system proxy.https="$proxy"
+    sudo snap install code --classic
+    sudo snap refresh code
+    echo "[INFO:VSCODE] Install extension 'Settings Sync'."
+}
+
+setup_chromium() {
+    sudo apt install -y chromium-browser
+}
+
+main
